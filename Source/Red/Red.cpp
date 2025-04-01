e18d4a1de5005b8a3e0a57d6ff9ce632e12a4038
jorge.marin1 <jorge.marin1@live.u-tad.com>
2025-04-01 18:22:24 +0200

    initial-commit

---
+++ Source/Red/Red.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Red.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Red, "Red" );
+ 
---
+++ Source/Red/RedGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RedGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARedGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARedGameMode();
+};
+
+
+
